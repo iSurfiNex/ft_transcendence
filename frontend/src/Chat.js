faeaaff323ac0ed215885f9d5c02105d48c135b5
Remi Sterin <banacraft@gmail.com>
2023-11-09 18:50:56 +0100

    chat layout dekstop done

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -7,11 +7,19 @@ import './Chat.css';
 
 
 function Chat() {
-	const [isChecked, setIsChecked] = useState(true);
 
-	const checkHandler = () => {
-		setIsChecked(!isChecked)
+	const [isBubbleChecked, setIsBubbleChecked] = useState(true);
+
+	const bubbleCheckHandler = () => {
+		setIsBubbleChecked(!isBubbleChecked);
 	}
+
+	const [isPlayerListChecked, setIsPlayerListChecked] = useState(true);
+
+	const playerListCheckHandler = () => {
+		setIsPlayerListChecked(!isPlayerListChecked);
+	}
+
 
 	const [mobile, setMobile] = useState(window.innerWidth <= 500);
 
@@ -26,18 +34,19 @@ function Chat() {
 		}
 	}, []);
 
+
 	const [mobileDefault, setMobileDefault] = useState(true);
 
-	if (!mobile && isChecked) {
-		setIsChecked(false);
+	if (!mobile && isBubbleChecked) {
+		setIsBubbleChecked(false);
 		setMobileDefault(true);
 	}
-	if (!mobile && !isChecked && !mobileDefault) {
-		setIsChecked(true)
+	if (!mobile && !isBubbleChecked && !mobileDefault) {
+		setIsBubbleChecked(true);
 		setMobileDefault(false);
 	}
-	if (mobile && !isChecked && mobileDefault) {
-		setIsChecked(true)
+	if (mobile && !isBubbleChecked && mobileDefault) {
+		setIsBubbleChecked(true);
 		setMobileDefault(false);
 	}
 
@@ -46,14 +55,14 @@ function Chat() {
 			<div class="chat-mobile">
 				<div class="chat-bubble">
 					<label class="btn btn-primary chat-bubble-label" for="btn-check">
-						<input onChange={checkHandler} checked={isChecked} type="checkbox" class="btn-check chat-bubble-check" id="btn-check" autocomplete="off"/>
+						<input onChange={bubbleCheckHandler} checked={isBubbleChecked} type="checkbox" class="btn-check chat-bubble-check" id="btn-check" autocomplete="off"/>
 						<img class="chat-bubble-unchecked" src={bubble} alt="bubble"/>
 						<img class="chat-bubble-checked" src={close} alt="close"/>
 					</label>
 				</div>
 			</div>
 
-			{!isChecked && (<div class="chat-desktop">
+			{!isBubbleChecked && (<div class="chat-desktop">
 				<div class="channels">
 					<div class="btn-group btn-group-toggle" data-toggle="buttons">
 						<label class="btn btn-secondary channels-bubble active">
@@ -82,34 +91,139 @@ function Chat() {
 						</label>
 					</div>
 				</div>
-				<div class="channels-separator"></div>
 
-				<form action="">
+				<div class="messages">
+					<div class="message">
+						<img class="message-player-img" src={list} alt="profile"></img>
+						<div class="message-player-date">18:05</div>
+						<div class="message-player-name">teeeeeeeeeeeeest</div>
+						<div class="message-player-content">Saqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqlut a tous</div>
+					</div>
+					<div class="message">
+						<img class="message-player-img" src={list} alt="profile"></img>
+						<div class="message-player-date">18:05</div>
+						<div class="message-player-name">teeeeeeeeeeeeest</div>
+						<div class="message-player-content">Saqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqlut a tous</div>
+					</div>
+					<div class="message">
+						<img class="message-player-img" src={list} alt="profile"></img>
+						<div class="message-player-name">teeeeeeeeeeeeest</div>
+						<div class="message-player-date">18:05</div>
+						<div class="message-player-content">Salut a tous</div>
+					</div>
+					<div class="message">
+						<img class="message-player-img" src={list} alt="profile"></img>
+						<div class="message-player-name">teeeeeeeeeeeeest</div>
+						<div class="message-player-date">18:05</div>
+						<div class="message-player-content">Salut a tous</div>
+					</div>
+				</div>
+
+				<div class="bottom-bar">
 					<input placeholder="Ecrivez votre message ici" class="form-control chat-input"/>
 					<label class="btn btn-primary chat-send" for="btn-check">
 						<div class="chat-send-button">
 							<input class="chat-send-img" alt="send" type="image" src={send} name="submit"/>
 						</div>
 					</label>
-				</form>
-				<div class="btn-group-toggle player-list" data-toggle="buttons">
-					<label class="btn btn-secondary active player-list-label">
-						<input type="checkbox" checked autocomplete="off" onClick=""/>
-						<img class="player-list-unchecked" src={list} alt="list"/>
-						<img class="player-list-checked" src={close} alt="close"/>
-					</label>
+					<div class="player-list">
+						<label class="btn btn-primary player-list-label" for="btn-check-list">
+							<input class="btn-check player-list-input" onChange={playerListCheckHandler} checked={isPlayerListChecked} type="checkbox" id="btn-check-list" autocomplete="off"/>
+							<img class="player-list-unchecked" src={list} alt="list"/>
+							<img class="player-list-checked" src={close} alt="close"/>
+						</label>
+					</div>
 				</div>
+
+				{!isPlayerListChecked && (<div class="chat-player-list">
+					<span class="chat-player-list-header-text">Player list</span>
+					<div class="chat-list-player">
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<div class="chat-player-name">teeeeeeeeeeeeest</div>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+							<div class="chat-player-seperator"></div>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+						<div class="chat-player">
+							<a class="chat-player-link" href="/">
+								<img class="chat-player-img" src={list} alt="profile"></img>
+								<span class="chat-player-name">test</span>
+							</a>
+							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
+							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+						</div>
+					</div>
+				</div>)}
 			</div>)}
 		</div>
 	);
 }
 
-function App() {
-	return (
-		<div class="App">
-			<Chat />
-		</div>
-	);
-}
-
-export default App;
+export default Chat;
