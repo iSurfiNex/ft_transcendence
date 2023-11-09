b1a9b4f699ed337b9b6ab0f6dcf5e1f04c5362c5
iSurfiNex <banacraft@gmail.com>
2023-11-09 23:24:29 +0100

    add notification

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -2,6 +2,9 @@ import list from './img/list.svg';
 import bubble from './img/bubble.svg';
 import close from './img/close.svg';
 import send from './img/send.svg';
+import plus from './img/plus.svg';
+import message from './img/message.svg';
+import block from './img/block.svg';
 import { useState, useEffect} from 'react';
 import './Chat.css';
 
@@ -64,164 +67,66 @@ function Chat() {
 
 			{!isBubbleChecked && (<div class="chat-desktop">
 				<div class="channels">
-					<div class="btn-group btn-group-toggle" data-toggle="buttons">
-						<label class="btn btn-secondary channels-bubble active">
-							<input type="radio" name="options" id="option1" autocomplete="off" checked/>G
+					<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" defaultChecked/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio1">G
+							<div class ="channels-bubble-notif"></div>
 						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>T
+
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off"/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio2">T
+							<div class ="channels-bubble-notif active">1</div>
 						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>
+
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off"/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio3">
+							<div class ="channels-bubble-notif"></div>
 						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>
+
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off"/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio4">
+							<div class ="channels-bubble-notif active">4</div>
 						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>
+
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off"/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio5">
+							<div class ="channels-bubble-notif active">1</div>
 						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>
+
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio6" autocomplete="off"/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio6">
+							<div class ="channels-bubble-notif active">+9</div>
 						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>
-						</label>
-						<label class="btn btn-secondary channels-bubble">
-							<input type="radio" name="options" id="option2" autocomplete="off"/>
+
+						<input type="radio" class="btn-check" name="btnradio" id="btnradio7" autocomplete="off"/>
+						<label class="btn btn-secondary channels-bubble" for="btnradio7">
+							<div class ="channels-bubble-notif"></div>
 						</label>
 					</div>
 				</div>
 
 				<div class="messages">
 					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-date">18:05</div>
+						<a href="/">
+							<img class="message-player-img" src={list} alt="profile"></img>
+							<div class="message-player-date">18:05</div>
+						</a>
 						<div class="message-player-name">teeeeeeeeeeeeest</div>
 						<div class="message-player-content">Saqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqlut a tous</div>
 					</div>
 					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-date">18:05</div>
+						<a href="/">
+							<img class="message-player-img" src={list} alt="profile"></img>
+							<div class="message-player-date">18:05</div>
+						</a>
 						<div class="message-player-name">teeeeeeeeeeeeest</div>
 						<div class="message-player-content">Saqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqlut a tous</div>
 					</div>
 					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
-						<div class="message-player-date">18:05</div>
-						<div class="message-player-content">Salut a tous</div>
-					</div>
-					<div class="message">
-						<img class="message-player-img" src={list} alt="profile"></img>
-						<div class="message-player-name">teeeeeeeeeeeeest</div>
+						<a href="/">
+							<img class="message-player-img" src={list} alt="profile"></img>
+							<div class="message-player-name">teeeeeeeeeeeeest</div>
+						</a>
 						<div class="message-player-date">18:05</div>
 						<div class="message-player-content">Salut a tous</div>
 					</div>
@@ -249,10 +154,11 @@ function Chat() {
 						<div class="chat-player">
 							<a class="chat-player-link" href="/">
 								<img class="chat-player-img" src={list} alt="profile"></img>
-								<div class="chat-player-name">teeeeeeeeeeeeest</div>
+								<div class="chat-player-name">teeeeeeeeeeeeeeeeeest</div>
 							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+							<button class="chat-player-message btn btn-primary"><img class="chat-player-button-img" src={message} alt="send message"></img></button>
+							<button class="chat-player-invite btn btn-success"><img class="chat-player-button-img" src={plus} alt="invite"></img></button>
+							<button class="chat-player-block btn btn-danger"><img class="chat-player-button-img" src={block} alt="block"></img></button>
 							<div class="chat-player-seperator"></div>
 						</div>
 						<div class="chat-player">
@@ -260,72 +166,9 @@ function Chat() {
 								<img class="chat-player-img" src={list} alt="profile"></img>
 								<span class="chat-player-name">test</span>
 							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
-						</div>
-						<div class="chat-player">
-							<a class="chat-player-link" href="/">
-								<img class="chat-player-img" src={list} alt="profile"></img>
-								<span class="chat-player-name">test</span>
-							</a>
-							<button class="chat-player-invite btn btn-success btn-sm">Inviter</button>
-							<button class="chat-player-block btn btn-danger btn-sm">Bloquer</button>
+							<button class="chat-player-message btn btn-primary"><img class="chat-player-button-img" src={message} alt="send message"></img></button>
+							<button class="chat-player-invite btn btn-success"><img class="chat-player-button-img" src={plus} alt="invite"></img></button>
+							<button class="chat-player-block btn btn-danger"><img class="chat-player-button-img" src={block} alt="block"></img></button>
 						</div>
 					</div>
 				</div>)}
