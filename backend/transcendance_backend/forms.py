76112f89d2922dda07a84074577108181b71bc6f
Félix JULLIEN <jullienfelix@gmail.com>
2023-11-10 15:02:33 +0100

    Django[API]: Add rest API endpoint generator

--- backend/transcendance_backend/forms.py
+++ backend/transcendance_backend/forms.py
@@ -1,8 +1,19 @@
 from django import forms
-from .models import Player
+from .models import Player, Tournament
 
 
 class PlayerForm(forms.ModelForm):
     class Meta:
         model = Player
         fields = ["name"]
+
+
+class TournamentForm(forms.ModelForm):
+    class Meta:
+        model = Tournament
+        fields = [
+            "pools",
+            "created_by",
+            "required_player_number",
+            "joined_players",
+        ]
