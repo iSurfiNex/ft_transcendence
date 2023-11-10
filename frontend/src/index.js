9489f766660ab4ea590a7de0c69b9bd5fea310e3
Remi Sterin <banacraft@gmail.com>
2023-11-10 16:17:05 +0100

    add topbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Chat from './Chat';
+import Topbar from './Topbar';
+import Layout from './Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Topbar />
+    <Layout />
     <Chat />
   </React.StrictMode>
 );
