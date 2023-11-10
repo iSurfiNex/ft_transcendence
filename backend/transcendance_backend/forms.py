ef4cb4ce06408d3a31b4396eb0f2e7fdc26cfbef
Félix JULLIEN <jullienfelix@gmail.com>
2023-11-10 02:20:37 +0100

    Django: update model, validation, api

---
+++ backend/transcendance_backend/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Player
+
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = ["name"]
--- backend/transcendance_backend/views.py
+++ backend/transcendance_backend/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
+from django.db.utils import IntegrityError
 from django.views import View
-from .models import Player
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
+
+from .models import Player
+from .forms import PlayerForm
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -12,52 +15,55 @@ class PlayerView(View):
         try:
             if id:
                 player = Player.objects.get(id=id)
-                data = {
-                    "id": player.id,
-                    "name": player.name,
-                    "xp": player.xp,
-                }
-                return JsonResponse(data)
+                response = player.serialize()
+                return JsonResponse(response, safe=False)
             else:
                 players = Player.objects.all()
-                data = [
-                    {"id": player.id, "name": player.name, "xp": player.xp}
-                    for player in players
-                ]
-                return JsonResponse(data, safe=False)
+                response = [player.serialize() for player in players]
+                return JsonResponse(response, safe=False)
         except Player.DoesNotExist:
-            return JsonResponse({"error": "Player not found"}, status=404)
+            return JsonResponse({"errors": "Player not found"}, status=404)
 
     def post(self, request):
         try:
             data = json.loads(request.body)
-            name = data["name"]
-            xp = data["xp"]
-            player = Player.objects.create(name=name, xp=xp)
-            return JsonResponse(
-                {"id": player.id, "name": player.name, "xp": player.xp}, status=201
-            )
+            form = PlayerForm(data)
+            if not form.is_valid():
+                return JsonResponse({"errors": form.errors})
+            new_player = Player(**form.cleaned_data)
+            new_player.save()
+            created_player = Player.objects.get(id=new_player.id)
+            return JsonResponse(created_player.serialize(), status=201, safe=False)
         except KeyError:
-            return JsonResponse({"error": "Invalid data"}, status=400)
+            return JsonResponse({"errors": "Invalid data"}, status=400)
+        except IntegrityError as e:
+            return JsonResponse({"errors": str(e)}, status=400)
         except json.JSONDecodeError as e:
-            return JsonResponse({"error": str(e)}, status=400)
+            return JsonResponse({"errors": str(e)}, status=400)
 
     def put(self, request, id):
         try:
             player = Player.objects.get(id=id)
-            data = json.loads(request.body)
-            if "name" in data:
-                player.name = data["name"]
-            if "xp" in data:
-                player.xp = data["xp"]
+            put_data = json.loads(request.body)
+            # Merge the current player attribute with the put data
+            updated_player_data = player.__dict__ | put_data
+            # By giving the instance, we prevent running into "already exist" validation error
+            form = PlayerForm(updated_player_data, instance=player)
+            if not form.is_valid():
+                return JsonResponse({"errors": form.errors})
+            for key, value in form.cleaned_data.items():
+                setattr(player, key, value)
             player.save()
-            return JsonResponse({"id": player.id, "name": player.name, "xp": player.xp})
+            updated_player = Player.objects.get(id=player.id)
+            return JsonResponse(updated_player.serialize(), status=200)
         except Player.DoesNotExist:
-            return JsonResponse({"error": "Player not found"}, status=404)
+            return JsonResponse({"errors": "Player not found"}, status=404)
+        except IntegrityError as e:
+            return JsonResponse({"errors": str(e)}, status=400)
         except KeyError:
-            return JsonResponse({"error": "Invalid data"}, status=400)
+            return JsonResponse({"errors": "Invalid data"}, status=400)
         except json.JSONDecodeError as e:
-            return JsonResponse({"error": str(e)}, status=400)
+            return JsonResponse({"errors": str(e)}, status=400)
 
     def delete(self, request, id):
         try:
@@ -65,4 +71,4 @@ class PlayerView(View):
             player.delete()
             return JsonResponse({}, status=204)
         except Player.DoesNotExist:
-            return JsonResponse({"error": "Player not found"}, status=404)
+            return JsonResponse({"errors": "Player not found"}, status=404)
