a45e3acd0d6137eecbbaeb07f0da697b9d08089f
Joshua Toulouse <jtoulous@3B3.42angouleme.fr>
2023-11-11 18:38:03 +0100

    wesh

--- frontend/src/pages/WaitingRoom.js
+++ frontend/src/pages/WaitingRoom.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "./WaitingRoom.css";
+import { Link } from "react-router-dom"
 
-export default function WaitingRoom() {
+function WaitingRoom() {
+	var toto = ["tonton_adolf6969", "oussama_sama", "miaKalifa", "augustAmes", "nikitaBelucci", "abellaDanger"]
+	var truc = toto.map((item)=>(<Link to="/profile"><div className="player"> {item} </div> </Link>))
+
 	return (
-	<>
-		<h1>WaitingRoom</h1>
-		<p>testing spa</p>
-	</>
+		<div className="background-waiting-room">
+			<div className="rectangle">
+				
+				<div className="title">
+					Waiting Room
+				</div>
+
+				<div className="players"> 
+					{truc}
+				</div>
+
+			</div>
+		</div>
 	);
 }
+
+export default WaitingRoom;
