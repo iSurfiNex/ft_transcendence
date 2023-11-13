edd0444d1253dccbecdc8214a87f929e2e9f2363
Remi Sterin <banacraft@gmail.com>
2023-11-13 18:06:16 +0100

    add Tournament and Othergame create/join list

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -4,9 +4,9 @@ import { Routes ,Route } from 'react-router-dom';
 import Game from "./pages/Game/Game"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import OtherGame from "./pages/OtherGame"
+import OtherGame from "./pages/OtherGame/OtherGame"
 import Profile from "./pages/Profile"
-import Tournament from "./pages/Tournament"
+import Tournament from "./pages/Tournament/Tournament"
 import WaitingRoom from "./pages/WaitingRoom"
 
 export default function Content() {
