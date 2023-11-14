b47bfb9911b811b75e9f64edf1d59194459305e6
iSurfiNex <banacraft@gmail.com>
2023-11-14 02:18:33 +0100

    better channel's picture background management

--- frontend/src/Chat/Channels.js
+++ frontend/src/Chat/Channels.js
@@ -15,11 +15,13 @@ function Channels() {
 		<div className="channels">
 			{globalData.jsonMap.channels.map((channel, index) => {
 				const uniqueId = `btnradio${idCounter++}`;
+				const channelUser = globalData.jsonMap.users.find(user => user.nickname === channel.name);
+				console.log(channelUser);
 
 				return (
 					<div key={index} className="btn-group" role="group" aria-label="Basic radio toggle button group">
 						<input type="radio" className="btn-check" name="btnradio" id={uniqueId} autoComplete="off" defaultChecked={channel.name === globalData.jsonMap.activeChannel} onChange={() => handleChannelChange(channel.name)}/>
-						<label className="btn btn-secondary channels-bubble" htmlFor={uniqueId} style={{ backgroundImage: `url(${require(`../${channel.picture}`)})`, backgroundSize: 'cover' }}>{channel.name.charAt(0)}
+						<label className="btn btn-secondary channels-bubble" htmlFor={uniqueId} style={channelUser ? { backgroundImage: `url(${require(`../${channelUser.picture}`)})`, backgroundSize: 'cover' } : {}}>{channel.name.charAt(0)}
 							<div className ="channels-bubble-notif"></div>
 						</label>
 					</div>
