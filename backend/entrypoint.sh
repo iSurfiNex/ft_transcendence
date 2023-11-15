63325285a95fbf86e4f4eb0e6c17a56057889a64
Remi Sterin <banacraft@gmail.com>
2023-11-15 20:08:44 +0100

    backend is working now

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+systemctl enable postgresql
+systemctl restart postgresql
+/etc/init.d/postgresql start
+
+echo "CREATE DATABASE transcendance_db; \
+CREATE ROLE my_postgres_user LOGIN; \
+ALTER ROLE my_postgres_user WITH PASSWORD 'my_postgres_pw';" | sudo -u postgres psql
+
+python3 manage.py createsuperuser
+python3 manage.py migrate
+python3 manage.py runserver
+
 tail -f
