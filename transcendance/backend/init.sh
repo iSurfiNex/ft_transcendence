b2ba39bf32ab76404c6d3939c129b1dceea33088
Joshua Toulouse <jtoulous@1E7.42angouleme.fr>
2023-11-16 10:21:57 +0100

    dock

--- transcendance/backend/init.sh
+++ transcendance/backend/init.sh
@@ -5,4 +5,4 @@ poetry install
 poetry run python3 manage.py createsuperuser
 poetry run python3 manage.py makemigrations
 poetry run python3 manage.py migrate 
-poetry run python3 manage.py runserverdocker
+poetry run python3 manage.py runserver
