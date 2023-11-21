d2f4f6154dcf5d0fd47d93a219bcbfb4261f587a
iSurfiNex <banacraft@gmail.com>
2023-11-21 19:06:56 +0100

    switch to new framework for the SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,47 @@
-import { useLocation } from "react-router-dom"
+import {setup} from "./pouic/state.js"
+import {evalRepeat} from './pouic/repeat_binding.js'
 
-import Chat from './Chat/Chat';
-import Topbar from './Topbar';
-import Layout from './Layout';
-import Content from './Content';
-
-export default function App() {
-    const { pathname } = useLocation();
-
-	return (
-		<>
-			{ pathname === "/login/" ? null : <Topbar />  }
-			{ pathname === "/login/" ? null : <Layout />  }
-			{ pathname === "/login/" ? null : <Chat />  }
-			<Content />
-		</>
-	)
+var state_base = {
+	users: [
+		{ nickname: 'rsterin' , fullname: 'Remi Sterin', picture: 'img/list.svg' },
+		{ nickname: 'fjullien' , fullname: 'Felix Jullien', picture: 'img/list.svg' },
+		{ nickname: 'jtoulous' , fullname: 'Joshua Toulouse', picture: 'img/list.svg' },
+		{ nickname: 'tlarraze' , fullname: 'Theo Larraze', picture: 'img/list.svg' },
+	],
+	whoAmI: 'rsterin',
+	isLoggedIn: true,
+	channels: [
+		{ name: 'Global'},
+		{ name: 'rsterin'},
+		{ name: 'fjullien'},
+		{ name: 'jtoulous'},
+		{ name: 'tlarraze'},
+	],
+	activeChannel: 'Global',
+	messages: [
+		{ text: 'Hello', sender: 'rsterin', date: '16:03', channel: 'fjullien' },
+		{ text: 'Hi there', sender: 'fjullien', date: '17:04', channel: 'Global' },
+		{ text: 'Greetings', sender: 'jtoulous', date: '18:05', channel: 'Global' },
+		{ text: 'Greetings', sender: 'tlarraze', date: '19:06', channel: 'Global' },
+	],
+	games: [
+		{ type: 'normal', id: '1', status: 'done', creator: 'fjullien', players: ['fjullien', 'tlarraze'], maxPlayer: '2'},
+		{ type: 'normal', id: '2', status: 'waiting', creator: 'fjullien', players: ['fjullien'], maxPlayer: '2'},
+		{ type: 'normal', id: '3', status: 'waiting', creator: 'fjullien', players: ['fjullien', 'tlarraze'], maxPlayer: '2'},
+		{ type: 'normal', id: '4', status: 'running', creator: 'fjullien', players: ['fjullien', 'tlarraze'], maxPlayer: '2'},
+		{ type: 'powerup', id: '5', status: 'done', creator: 'fjullien', players: ['fjullien', 'tlarraze'], maxPlayer: '2'},
+		{ type: 'powerup', id: '6', status: 'waiting', creator: 'fjullien', players: ['fjullien'], maxPlayer: '2'},
+		{ type: 'powerup', id: '7', status: 'waiting', creator: 'fjullien', players: ['fjullien', 'tlarraze'], maxPlayer: '2'},
+		{ type: 'powerup', id: '8', status: 'running', creator: 'fjullien', players: ['fjullien', 'tlarraze'], maxPlayer: '2'},
+		{ type: 'tournament', id: '9', status: 'done', creator: 'rsterin', players: ['rsterin', 'jtoulous', 'fjullien', 'tlarraze'], maxPlayer: '4'},
+		{ type: 'tournament', id: '10', status: 'waiting', creator: 'rsterin', players: ['rsterin', 'jtoulous', 'fjullien', 'tlarraze'], maxPlayer: '4'},
+		{ type: 'tournament', id: '11', status: 'running', creator: 'rsterin', players: ['rsterin', 'jtoulous', 'fjullien', 'tlarraze'], maxPlayer: '8'},
+		{ type: 'tournament', id: '12', status: 'waiting', creator: 'rsterin', players: ['rsterin', 'jtoulous', 'fjullien', 'tlarraze'], maxPlayer: '6'},
+	],
+	currentGame: '-1',
 }
+
+const state = setup(state_base)
+console.log(state)
+evalRepeat(document, window)
+
--- frontend/src/init_bootstrap_plugins.js
+++ frontend/src/init_bootstrap_plugins.js
@@ -1,12 +1,12 @@
 
 import { Toast, Popover } from 'bootstrap'
 
-export const initPopover = (scope) =>{
-  if (scope !== document)
-    scope = scope.shadowRoot
-  if (!scope){
-    console.warn('Invalid scope for Popover initialization')
-    return
-  }
-  [...scope.querySelectorAll('[data-bs-toggle="popover"]')].map(el => new Popover(el))
+export const initPopover = (scope) => {
+    if (scope !== document)
+        scope = scope.shadowRoot
+    if (!scope) {
+        console.warn('Invalid scope for Popover initialization')
+        return
+    }
+    [...scope.querySelectorAll('[data-bs-toggle="popover"]')].map(el => new Popover(el))
 }
