a483aeca56f1356377e78b9eaed22f9d8982cedc
Remi Sterin <banacraft@gmail.com>
2023-11-22 16:55:22 +0100

    add isMobile

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,10 @@
 function navigateTo(path) {
 	history.pushState({}, "", path);
 }
+
+function checkScreenWidth() {
+	isMobile = (window.innerWidth < 768 || window.innerHeight < 524);
+}
+
+checkScreenWidth();
+window.addEventListener('resize', checkScreenWidth);
