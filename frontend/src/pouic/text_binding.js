c280293566c15b2d49782213801b36262477332b
Félix JULLIEN <jullienfelix@gmail.com>
2023-11-23 03:30:31 +0100

    Add text binding

--- frontend/src/pouic/text_binding.js
+++ frontend/src/pouic/text_binding.js
@@ -1,11 +1,11 @@
-import {extractPathScope, addPathObserver, get_prop} from './utils.js'
+import {extractPathScope, addPathObserver, get_prop, bracketEval} from './utils.js'
 
 // Function to split a text node based on a regex pattern
 export const bindTextNode = (scope, prefixes, textNode) => {
   const text = textNode.nodeValue;
   // TODO i've quickly added ":" to ignore css property between brackets, there should be a better way
   //const regexPattern = /\{([^}:]+)\}/g; // Regex to match text inside curly braces
-  const regexPattern = /\{\s*([a-zA-Z0-9_.]+)\s*\}/;
+  const regexPattern = /(\{\s*[a-zA-Z0-9_.\(\)\,\)]+\s*\})/;
   const matches = text.split(regexPattern);
   if (matches.length < 2)
     return
@@ -19,19 +19,13 @@ export const bindTextNode = (scope, prefixes, textNode) => {
     if (index % 2 === 1) {
       // This is a match (text inside brackets),
 
-      let path = part.split('.')
-      let varScope = extractPathScope(path, scope, prefixes)
-      //let newTextNode = document.createTextNode(get_prop(varScope, path));
-      //textNode.parentNode.insertBefore(newTextNode, textNode.nextSibling);
-      parentNode.insertAdjacentText("beforeend", get_prop(varScope, path));
+      parentNode.insertAdjacentText("beforeend", "")
       let newTextNode = parentNode.lastChild
-      let onchange = (varVal) => { newTextNode.nodeValue = varVal; }
-      addPathObserver(path, onchange)
+      let onTextChange = (varVal) => { newTextNode.nodeValue = varVal; }
+      bracketEval(part, scope, prefixes, onTextChange)
     }
     else {
       parentNode.insertAdjacentText("beforeend", part);
-      //let otherTextNode = document.createTextNode(part);
-      //parentNode.insertBefore(otherTextNode, textNode.nextSibling);
     }
 
   });
