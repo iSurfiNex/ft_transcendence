f4491989ddf522e09dba5e69a59c7494db94b9ee
Félix JULLIEN <jullienfelix@gmail.com>
2023-11-24 19:17:44 +0100

    add nginx conf

--- backend/transcendance_backend/settings.py
+++ backend/transcendance_backend/settings.py
@@ -26,6 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
