1768c1462850ffdc9b703c1a21e9bbf520d13e98
Remi Sterin <banacraft@gmail.com>
2023-11-24 16:10:05 +0100

    entrypoint for django now work

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,8 +8,8 @@ echo "CREATE DATABASE transcendance_db; \
 CREATE ROLE my_postgres_user LOGIN; \
 ALTER ROLE my_postgres_user WITH PASSWORD 'my_postgres_pw';" | sudo -u postgres psql
 
-python3 manage.py createsuperuser
 python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py createsuperuser --noinput
+python3 manage.py runserver 0.0.0.0:8000
 
 tail -f
