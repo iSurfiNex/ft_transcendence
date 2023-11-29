9c860dd1a56705c70108fff63276f0fc2037eb3d
Remi Sterin <banacraft@gmail.com>
2023-11-29 19:36:27 +0100

    login done, missing last request to update state

--- backend/transcendance_backend/static/pages/Login.js
+++ backend/transcendance_backend/static/pages/Login.js
@@ -7,8 +7,6 @@ class PongLogin extends Component {
 	static template = html`
 	<div>
 		<button @click="this.handleClick()">Connect</button>
-		<button @click="this.handlePostRequest()">PostRequest</button>
-		<p>{type}</p>
 	</div>
 `
 
@@ -32,52 +30,27 @@ class PongLogin extends Component {
 
 	connectedCallback() {
 		initPopover(this)
+
+		const type = new URLSearchParams(window.location.search).get("code");
+		console.log(type);
+		if (type) {
+			const hostname = window.location.origin + '/api/requestlogin/?code=' + type
+			const response = fetch(hostname, {
+				method: 'GET',
+			}).then(async (response)=>{
+				const resp = await response.json()
+				console.log(resp)
+				sessionStorage.setItem("access_token", resp.access_token);
+			})
+		}
 	}
 
 	handleClick() {
-		const apiUrl = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-fe7d42984dd6575235bba558210f67f242c7853d17282449450969f21d6f9080&redirect_uri=https%3A%2F%2Flocalhost%3A8000%2Flogin%2F&response_type=code';
+		const hostname = encodeURIComponent(window.location.origin + '/login/')
+		const apiUrl = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-fe7d42984dd6575235bba558210f67f242c7853d17282449450969f21d6f9080&redirect_uri=' + hostname + '&response_type=code';
 
 		window.location.href = apiUrl;
 	}
-
-	handlePostRequest = async () => {
-		const type = new URLSearchParams(window.location.search).get("code");
-
-		const postData = {
-			grant_type: 'authorization_code',
-			client_id: 'u-s4t2ud-fe7d42984dd6575235bba558210f67f242c7853d17282449450969f21d6f9080',
-			client_secret: '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',
-			code: type, // Replace {type} with the actual value
-			redirect_uri: 'https://localhost:8000/login/',
-		};
-		const formData = new FormData();
-		formData.append("grant_type", "authorization_code");
-		formData.append("client_id", "u-s4t2ud-fe7d42984dd6575235bba558210f67f242c7853d17282449450969f21d6f9080");
-		formData.append("client_secret", "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");
-		formData.append("code", type);
-		formData.append("redirect_uri", "https://localhost:8000/login/");
-		try {
-				const response = await fetch('https://api.intra.42.fr/oauth/token', {
-				method: 'POST',
-				headers: {
-					// 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-					'Access-Control-Allow-Origin': 'https://api.intra.42.fr',
-				},
-				mode: 'cors',
-				body: formData,
-			});
-
-			console.log(response)
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-
-			const data = await response.json();
-			console.log(data);
-		} catch (error) {
-				console.error('There was a problem with the fetch operation:', error);
-			}
-		};
 }
 
 register(PongLogin)
