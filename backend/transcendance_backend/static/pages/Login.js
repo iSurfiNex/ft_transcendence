7f11e351634a9945ad5490e02440835fbcd1c3f0
iSurfiNex <banacraft@gmail.com>
2023-11-30 00:12:00 +0100

    update front for login

--- backend/transcendance_backend/static/pages/Login.js
+++ backend/transcendance_backend/static/pages/Login.js
@@ -5,8 +5,29 @@ import { bootstrapSheet } from '/static/bootstrap/bootstrap_css.js'
 class PongLogin extends Component {
 	static sheets = [bootstrapSheet]
 	static template = html`
-	<div>
-		<button @click="this.handleClick()">Connect</button>
+	<div class="login">
+		<div class="input">
+			<div class="login-register">
+				<form class="form-login">
+					<input type="text" placeholder="Username" class="input-field">
+					<input type="password" placeholder="Password" class="input-field">
+					<button id="login-button" class="input-button pushable">
+						<span class="front">Login</span>
+					</button>
+				</form>
+				<form class="form-register">
+					<input type="text" placeholder="Username" class="input-field">
+					<input type="password" placeholder="Password" class="input-field">
+					<button id="register-button" class="input-button pushable">
+						<span class="front">Register</span>
+					</button>
+				</form>
+			</div>
+			<span class="separator">―――――――――――――――――</span>
+			<button id="pong-button" @click="this.handleClick()" class="pushable">
+				<span class="front">Connexion with 42</span>
+			</button>
+		</div>
 	</div>
 `
 
@@ -21,6 +42,81 @@ class PongLogin extends Component {
 
 	@media only screen and (min-width: 768px) and (min-height: 524px) {
 
+	}
+
+	.login {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	.input {
+		display: flex;
+		flex-direction: column;
+		padding: 20px;
+		background-color: gray;
+		border-radius: 20px;
+		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+	}
+
+	.login-register {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.form-login {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.form-register {
+		display: flex;
+		flex-direction: column;
+		margin-left: 25px;
+	}
+
+	.input-field {
+		padding: 10px;
+		margin-bottom: 15px;
+		border: none;
+		border-radius: 8px;
+	}
+
+	.input-button {
+		margin-top: 5px;
+		margin-bottom: 15px;
+	}
+
+	.separator {
+		color: #6e6e6e;
+		text-align: center;
+		margin-bottom: 15px;
+	}
+
+	.pushable {
+		background: hsl(130, 100%, 32%);
+		border-radius: 12px;
+		border: none;
+		padding: 0;
+		cursor: pointer;
+		outline-offset: 4px;
+	}
+
+	.front {
+		display: block;
+		padding: 12px 42px;
+		border-radius: 12px;
+		font-size: 1.25rem;
+		background: hsl(123, 100%, 39%);
+		color: white;
+		transform: translateY(-6px);
+		font-family: 'Press Start 2P', sans-serif;
+	}
+
+	.pushable:active .front {
+		transform: translateY(-2px);
 	}
 `
 
