cbc8979eb8f67421baf1644c7d490cba4dbd8696
Remi Sterin <banacraft@gmail.com>
2023-11-29 17:15:31 +0100

    change path for browser-sync

--- backend/transcendance_backend/bs-config.js
+++ backend/transcendance_backend/bs-config.js
@@ -2,7 +2,7 @@ const historyFallback = require('connect-history-api-fallback');
 
 module.exports = {
 	server: {
-		baseDir: "./", // ou spécifie le chemin vers ton dossier racine
+		baseDir: "./static/", // ou spécifie le chemin vers ton dossier racine
 	},
 	files: [ "**/*" ],
 	middleware: [historyFallback()], // Enable SPA routing
