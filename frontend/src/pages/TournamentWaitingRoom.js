25055eacfaf229f1320426f5cf9853783c33423a
jtoulous <toulouse.joshua97@gmail.com>
2023-12-03 00:19:30 +0100

    wr-css

--- frontend/src/pages/TournamentWaitingRoom.js
+++ frontend/src/pages/TournamentWaitingRoom.js
@@ -10,7 +10,11 @@ class TournamentWaitingRoom extends Component {
         <div class="rectangle-waiting-room">
 			<div class="title"> Waiting Room </div>
             <div class="countdown"> STARTING IN n  </div>
-		</div>
+            
+            <div class="match-list"> </div>
+            </div>                
+		
+            </div>
 	</div>
 `
     static css= css`
@@ -37,46 +41,7 @@ class TournamentWaitingRoom extends Component {
         }
 
         .title {
-            color: rgb(254, 254, 254);
-            font-size: 4vh;
-            width: 90%;
-            left: 5%;
-            top: 10%;
             position: absolute;
-            text-align: center;
-        }
-
-        .countdown {
-
-        }
-    }
-
-    @media only screen and (max-height: 524px) {
-        .background-waiting-room {
-            position: absolute;
-            right: 0;
-            bottom: 0;
-            width: calc(75% - 10px);
-            height: calc(90% - 10px);
-            background-color: rgba(255, 255, 255, 0.5);
-        }
-
-        .rectangle-waiting-room {
-            position: absolute;
-            bottom: 10%;
-            width: 50%;
-            height: 80%;
-            left: 25%;
-            background-color: rgba(200, 200, 200, 0.1);
-            box-shadow: rgba(0, 0, 0, 0.7) 0px 5px 15px;
-            backdrop-filter: blur(1px);
-            display: flex;
-            align-items: center;
-        }
-
-        .title {
-            position: absolute;
-            color: rgb(254, 254, 254);
             font-size: 8vh;
             width: 100%;
             height: 20%;
@@ -85,16 +50,34 @@ class TournamentWaitingRoom extends Component {
             top: 0%;
             overflow: hidden;
             text-align: center;
+            
+            font-family: 'Press Start 2P', sans-serif;
+            font-size: 8vh;
+            color: #FFA500;
+            text-shadow: 
+                2px 2px 3px #000000,
+                4px 4px 6px #000000,
+                6px 6px 9px #000000;
+            text-align: center;
         }
 
         .countdown {
-            
+            position: absolute;
+            height: 10%;
+            width: 100%;
+            top: 20%;
+            font-family: 'Courier New', monospace;
+            font-size: 3.5vh;
+            color: #00ff00;
+            text-shadow: 
+                2px 2px 3px #009900,
+                4px 4px 6px #006600,
+                6px 6px 9px #003300;
+            text-align: center;
         }
-
     }
 
-
-    @media only screen and (min-width: 768px) and (min-height: 524px) { 
+    @media only screen and (max-height: 524px) {
         .background {
             position: absolute;
             right: 0;
@@ -138,13 +121,13 @@ class TournamentWaitingRoom extends Component {
             overflow: hidden;
             text-align: center;
             
-            font-family: 'Courier New', monospace;
+            font-family: 'Press Start 2P', sans-serif;
             font-size: 8vh;
-            color: #00ff00;
+            color: #FFA500;
             text-shadow: 
-                2px 2px 3px #009900,
-                4px 4px 6px #006600,
-                6px 6px 9px #003300;
+                2px 2px 3px #000000,
+                4px 4px 6px #000000,
+                6px 6px 9px #000000;
             text-align: center;
         }
 
@@ -154,13 +137,89 @@ class TournamentWaitingRoom extends Component {
             width: 100%;
             top: 20%;
             font-family: 'Courier New', monospace;
-            font-size: 1.5em;
+            font-size: 3.5vh;
             color: #00ff00;
             text-shadow: 
                 2px 2px 3px #009900,
                 4px 4px 6px #006600,
                 6px 6px 9px #003300;
             text-align: center;
+        }
+    }
+
+
+    @media only screen and (min-width: 768px) and (min-height: 524px) { 
+        .background {
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            width: calc(75% - 10px);
+            height: calc(90% - 10px);
+            background-color: rgba(255, 255, 255, 0.5);
+        }
+
+        .background-img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            filter: brightness(50%); 
+        }
+
+        .rectangle-waiting-room {
+            position: absolute;
+            bottom: 10%;
+            width: 50%;
+            height: 80%;
+            left: 25%;
+            background-color: rgba(200, 200, 200, 0.1);
+            box-shadow: rgba(0, 0, 0, 0.7) 0px 5px 15px;
+            backdrop-filter: blur(1px);
+            display: flex;
+            align-items: center;
+        }
+
+        .title {
+            position: absolute;
+            width: 100%;
+            height: 20%;
+            left: 0%;
+            right: 15%;
+            top: 0%;
+            overflow: hidden;
+            
+            font-family: 'Press Start 2P', sans-serif;
+            font-size: 7vh;
+            color: #FFA500;
+            text-shadow: 
+                2px 2px 3px #000000,
+                4px 4px 6px #000000,
+                6px 6px 9px #000000;
+            text-align: center;
+        }
+
+        .countdown {
+            position: absolute;
+            height: 10%;
+            width: 100%;
+            top: 20%;
+            font-family: 'Courier New', monospace;
+            font-size: 3.5vh;
+            color: #00ff00;
+            text-shadow: 
+                2px 2px 3px #009900,
+                4px 4px 6px #006600,
+                6px 6px 9px #003300;
+            text-align: center;
+        }
+
+        .match-list {
+            position: absolute;
+            height: 65%;
+            width: 100%;
+            top: 35%;
         }
     }
 
