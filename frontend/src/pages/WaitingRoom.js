aebbab8f15be544525ca895ac06c736cf1929881
jtoulous <toulouse.joshua97@gmail.com>
2023-12-02 16:40:57 +0100

    wr

--- frontend/src/pages/WaitingRoom.js
+++ frontend/src/pages/WaitingRoom.js
@@ -2,7 +2,7 @@ import { Component, register, html, css } from 'pouic'
 import { initPopover } from '/src/bootstrap/init_bootstrap_plugins.js'
 import { bootstrapSheet } from '/src/bootstrap/bootstrap_css.js'
 
-
+    
 class PongWaitingRoom extends Component {
 	static sheets = [bootstrapSheet]
 	static template = html`
@@ -51,17 +51,71 @@ class PongWaitingRoom extends Component {
 
         .title-waiting-room {
             color: rgb(254, 254, 254);
-            font-size: 2.5vh;
+            font-size: 4vh;
             width: 90%;
             left: 5%;
             top: 10%;
             position: absolute;
             text-align: center;
         }
+
+        .game-room {
+            position: absolute;
+            top: 30%;
+            width: 70%;
+            height: 70%;
+            left: 15%;
+            overflow-y: auto;
+        }
+
+        .player-list{
+            position: absolute;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            overflow-y: auto;
+        }
+
+        .player {
+            position: relative;
+            flex-direction: column;
+            width: 100%;
+            height: 7%;
+        }
+
+        .profil {
+            position: relative;
+            flex-direction: column;
+            width: 100%;
+            height: 100%;
+            margin: 10%;
+        }
+
+        .profil-pic {
+            position: absolute;
+            left: 0%;
+            height: 100%;
+            width 25%;
+        }
+
+        .profil-nick {
+            position: absolute;
+            font-size: 2.5vh;
+            width: 40%;
+            height: 50%;
+            top: 25%;
+            left: 35%;
+        }
+
+        a {
+            color: white;
+            text-decoration: none;
+        }
     }
 
 
-    @media (hover: hover) {
+    @media only screen and (max-height: 524px) {
         .background-waiting-room {
             position: absolute;
             right: 0;
@@ -149,6 +203,110 @@ class PongWaitingRoom extends Component {
             color: white;
             text-decoration: none;
         }
+    }
+
+	@media only screen and (min-width: 768px) and (min-height: 524px) {
+        .background-waiting-room {
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            width: calc(75% - 10px);
+            height: calc(90% - 10px);
+            background-color: rgba(255, 255, 255, 0.5);
+        }
+
+        .rectangle-waiting-room {
+            position: absolute;
+            bottom: 10%;
+            width: 50%;
+            height: 80%;
+            left: 25%;
+            background-color: rgb(54, 54, 54);
+            box-shadow: rgba(0, 0, 0, 0.7) 0px 5px 15px;
+            display: flex;
+            align-items: center;
+        }
+
+        .title-waiting-room {
+            position: absolute;
+            color: rgb(254, 254, 254);
+            font-size: 8vh;
+            width: 100%;
+            height: 20%;
+            left: 0%;
+            right: 15%;
+            top: 0%;
+            overflow: hidden;
+            text-align: center;
+        }
+
+        .game-room {
+            position: absolute;
+            top: 20%;
+            width: 50%;
+            height: 80%;
+            left: 25%;
+            overflow-y: auto;
+            overflow-x: hidden;
+        }
+
+        .player-list{
+            position: absolute;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            overflow-y: auto;
+        }
+
+        .player {
+            position: relative;
+            flex-direction: column;
+            width: 100%;
+            height: 15%;
+        }
+
+        .profil {
+            position: relative;
+            flex-direction: column;
+            width: 100%;
+            height: 100%;
+            margin: 10%;
+        }
+
+        .profil-pic {
+            position: absolute;
+            left: 0%;
+            height: 100%;
+            width 25%;
+        }
+
+        .profil-nick {
+            position: absolute;
+            font-size: 2.5vh;
+            width: 40%;
+            height: 50%;
+            top: 25%;
+            left: 35%;
+        }
+
+        a {
+            color: white;
+            text-decoration: none;
+        }
+    }
+
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        display: none;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        display: none;
     }
     `
 
