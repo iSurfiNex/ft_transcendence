103e414a68793f910538991262a5f7cdfb4ee842
Félix JULLIEN <jullienfelix@gmail.com>
2024-02-11 11:36:42 +0100

    pongpage: fix startin

--- backend/transcendance_backend/static/pages/PongGame.js
+++ backend/transcendance_backend/static/pages/PongGame.js
@@ -192,7 +192,7 @@ class PongGame extends Component {
 
   constructor() {
     super();
-    observe("game.startedAt", this.updatedStartIn.bind(this));
+    this.updatedStartIn(state.game.started_at);
   }
 
   giveUp() {
